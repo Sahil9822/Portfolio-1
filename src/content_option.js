197ab7531ca145c462b06d1113ce64d1330cecdf
Sahil Pawale <sahilpawale444@gmail.com>
2023-06-19 22:00:53 +0530

    initial commit

---
+++ src/content_option.js
@@ -0,0 +1,111 @@
+const logotext = "SAHIL PAWALE";
+const meta = {
+    title: "Sahil Pawale",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Sahil Pawale",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop machine learning",
+    },
+    description: "I am pursuing B-Tech student at VIT Bhopal's Computer Science Department. I was born and raised in Maharashtra. I like to read historical books. I believe history teaches us a lot about different mistakes and achievements of the past, which can help us make informed decisions in the present and shape a better future.",
+    your_img_url: "https://drive.google.com/file/d/1mJjP9JFQpE7la2JQSZEb9xCKf5VyjM5q/view?usp=drive_link",
+};
+
+const dataabout = {
+    title: "Python Developer & Web Developer.",
+    aboutme: "I'm a Python and Web Developer with one year of experience in developing web applications using a variety of technologies. I have experience working with both front-end and back-end development and am proficient in HTML, CSS, JavaScript, and PHP. I also have experience with popular web frameworks such as Django and Flask. In addition to web development, I have a passion for data analysis and machine learning and have developed several applications that utilise these technologies. I have contributed to several open source projects on Github and am committed to continuing to learn and grow as a developer. My goal is to leverage my skills and expertise to create innovative solutions that solve real-world problems and contribute to the development community.",
+};
+const worktimeline = [{
+        jobtitle: "Python Essentials",
+        where: "Vityarthi",
+        date: "July 2022",
+    },
+    {
+        jobtitle: "Introduction to Ethical Hacking",
+        where: "Vityarthi",
+        date: "July 2022",
+    },
+    {
+        jobtitle: "AWS Cloud Masterclass - Cloud Practitioner Essentials",
+        where: "Futureskills Prime",
+        date: "April 2022",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Djano",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "HTML & CSS",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Bachelor of Technology(Computer Science & Engineering)",
+        description: "Vellore Institute of Technology, Bhopal, India, CGPA:- 6.63, 2020 - Present",
+    },
+    {
+        title: "Class 12th",
+        description: "Sri Chaitanya Junior College, Pune, India, Percentage:- 71.08, 2019 - 2020",
+    },
+    {
+        title: "Class 10th",
+        description: "K.T.E.S English Medium School, Rajgurunagar, India, Percentage:- 82.00, 2017 - 2018",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://drive.google.com/file/d/1LB6kNGk7AJ9CNTFih3Nr3DFuSeJypdpL/view?usp=sharing",
+        description: "The objective of the project is to create a machine learning model. We are doing a supervised learning and our aim is to do predictive analysis to predict housing price",
+        link: "https://github.com/Sahil9822/Pune-House-Price-Prediction",
+    },
+    {
+        img: "https://drive.google.com/file/d/1yx-EGXh4-8G0PsfdLelzVX3K3uDjarWe/view?usp=sharing",
+        description: "Water analysis using machine learning involves the application of statistical and artificial intelligence techniques to water quality data in order to extract insights, identify patterns, and make predictions.",
+        link: "https://github.com/Sahil9822/Water-Analysis",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "sahil.ganpat2020@vitbhopal.ac.in",
+    YOUR_FONE: "+91 9699395480",
+
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Sahil9822",
+    linkedin: "https://www.linkedin.com/in/sahil-pawale-b21214224/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
